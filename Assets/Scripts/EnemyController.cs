9be9b486205905dbe293aaa29115b0b580ab68d1
Alex Crawford <alex.crawford@codingzeal.com>
2016-04-16 22:32:57 -0700

    Enemy sprite and add horizontal raycast

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,7 +12,7 @@ public class EnemyController : MonoBehaviour {
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
         MoveEnemy();
@@ -29,20 +29,43 @@ public class EnemyController : MonoBehaviour {
 
     void CheckForEdge()
     {
+        RaycastHit2D hitright = Physics2D.Raycast(transform.position, Vector2.right, gameObject.GetComponent<Renderer>().bounds.size.x/2 + .01f);
+        if (hitright.collider != null)
+        {
+          CheckCollider(hitright);
+        }
+
+        RaycastHit2D hitleft = Physics2D.Raycast(transform.position, Vector2.left, gameObject.GetComponent<Renderer>().bounds.size.x/2 + .01f);
+        if (hitleft.collider != null)
+        {
+          CheckCollider(hitleft);
+        }
+
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, gameObject.GetComponent<Renderer>().bounds.size.y/2 + .01f); //transform.lossyScale.y/2 +
         if (hit.collider == null)
         {
-            if (isMovingRight == false)
-                isMovingRight = true;
-            else
-                isMovingRight = false;
+            ChangeDirection();
             rb2d.isKinematic = false;
         }
         else if (hit.collider.tag == "Ground" || hit.collider.tag == "Platform")
         {
             rb2d.isKinematic = true;
         }
-            
+    }
 
+    void CheckCollider(RaycastHit2D hitarea)
+    {
+      if (hitarea.collider.tag == "Ground")
+      {
+          ChangeDirection();
+      }
+    }
+
+    void ChangeDirection()
+    {
+      if (isMovingRight == false)
+          isMovingRight = true;
+      else
+          isMovingRight = false;
     }
 }
